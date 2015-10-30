cf1185c1151fba3ae8fcd96423c0e2c7396719e7
Sanqiang Zhao <zhaosanqiang916@gmail.com>
2015-10-30 13:52:05 -0800

    add multi-label support

--- io_util.py
+++ io_util.py
@@ -1,44 +1,63 @@
 from xml.dom import minidom
-from sklearn.feature_extraction.text import CountVectorizer,HashingVectorizer,TfidfVectorizer
+from sklearn import metrics
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.multiclass import OneVsRestClassifier
 from sklearn.linear_model import LogisticRegression
-from sklearn.ensemble import RandomForestClassifier,AdaBoostClassifier
 from sklearn import cross_validation
 import numpy as np
-from sklearn import metrics
-from sklearn.svm import SVC
 from passage.models import RNN
 from passage.updates import Adadelta
-from passage.layers import Embedding, GatedRecurrent, LstmRecurrent, Dense
-from passage.preprocessing import Tokenizer
-from sklearn.metrics import roc_auc_score
+from passage.layers import Embedding, GatedRecurrent, Dense
 from sklearn.preprocessing import MultiLabelBinarizer
 
 fname = "data/2007ChallengeTrainData.xml"
 docu = minidom.parse(fname)
 docs = docu.getElementsByTagName("doc")
 
-x=[]
-y=[]
+x = []
+y = []
+
+
+def transfer_multilabel(y_predict, y_text):
+    y_predict_new = []
+    y_text_new = []
+    n_record = y_predict.shape[0]
+    for idx in range(0, n_record):
+        y_predict_record = y_predict[idx]
+        y_text_record = y_text[idx]
+        find_items = np.where(True == np.logical_and(y_predict_record, y_text_record))
+        if len(find_items[0]) > 0:
+            y_predict_new.append(find_items[0][0])
+            y_text_new.append(find_items[0][0])
+        else :
+            y_predict_new.append(0)
+            y_text_new.append(np.where(1 == y_text_record)[0][0])
+    return y_predict_new, y_text_new
+
 
 for doc in docs:
     codes = doc.getElementsByTagName("code")
     texts = doc.getElementsByTagName("text")
-    if codes[0].getAttribute("origin") == "CMC_MAJORITY" and codes[1].getAttribute("origin") != "CMC_MAJORITY":
-        code_label = codes[0].firstChild.data
-        #if code_label == "786.2" or code_label == "599.0" or code_label == "593.70" or code_label == "780.6":
-        text_data  = ""
-        for text in texts:
-            text_data = " ".join([text_data, text.firstChild.data])
-        x.append(text_data)
-        y.append(code_label)
-        #print(text_data)
-        #print(code_label)
+
+    code_label = []
+    for code in codes:
+        if code.getAttribute("origin") == "CMC_MAJORITY":
+            code_label.append(code.firstChild.data)
+    y.append(code_label)
+
+    text_data = ""
+    for text in texts:
+        text_data = " ".join([text_data, text.firstChild.data])
+    x.append(text_data)
 
 
-#tv = TfidfVectorizer(x,stop_words='english',  min_df=0.00002)
-tv = CountVectorizer(x,strip_accents='ascii',ngram_range = (1,2), binary = True)
-#tv = HashingVectorizer(x,strip_accents='ascii',ngram_range = (1,1), binary = True)
-tfidf_train= tv.fit_transform(x)
+
+
+# tv = TfidfVectorizer(x,stop_words='english',  min_df=0.00002)
+tv = CountVectorizer(x, strip_accents='ascii', ngram_range=(1, 2), binary=True)
+# tv = HashingVectorizer(x,strip_accents='ascii',ngram_range = (1,1), binary = True)
+tfidf_train = tv.fit_transform(x)
+y = MultiLabelBinarizer().fit_transform(y)
 y = np.array(y)
 method = "trad"
 
@@ -46,24 +65,17 @@ scores = []
 report_y_actual = []
 report_y_predict = []
 
-kf = cross_validation.KFold(tfidf_train.shape[0], n_folds=10,shuffle=True)
+kf = cross_validation.KFold(tfidf_train.shape[0], n_folds=tfidf_train.shape[0], shuffle=True)
 for train_index, test_index in kf:
-    x_train, x_test = tfidf_train[train_index].toarray(),tfidf_train[test_index].toarray()
+    x_train, x_test = tfidf_train[train_index].toarray(), tfidf_train[test_index].toarray()
     y_train, y_test = y[train_index], y[test_index]
     if method == "trad":
-        model = LogisticRegression(C=100)
+        model = OneVsRestClassifier(LogisticRegression(C=978))
         model.fit(x_train, y_train)
-        predicty = model.predict(x_test)
-
-        #m_precision = metrics.precision_score(y_test,predicty)
-        #m_recall = metrics.recall_score(y_test,predicty)
-        #m_cls_report = metrics.classification_report(y_test, predicty)
-        report_y_predict.extend(predicty)
-        report_y_actual.extend(y_test)
-        #print(m_precision)
-        #print(m_recall)
-        #print(m_cls_report)
-        #scores.append((m_precision, m_recall))
+        y_predict = model.predict(x_test)
+        y_predict_new, y_text_new = transfer_multilabel(y_predict, y_test)
+        report_y_predict.extend(y_predict_new)
+        report_y_actual.extend(y_text_new)
     elif method == "rnn":
         layers = [
             Embedding(size=256, n_features=tfidf_train.shape[1]),
@@ -78,7 +90,5 @@ for train_index, test_index in kf:
         predY[pr_teX < 0.5] = -1
         print(score)
         scores.append(score)
-#print("Result:")
-#print(np.mean(scores))
 m_cls_report = metrics.classification_report(report_y_actual, report_y_predict)
 print(m_cls_report)
